d223a62f3ba043cdb3af9d84db5ef1f6e88a50d6
Veronika Kukk <veronika.kukk@ut.ee>
2025-05-02 19:40:05 +0300

    Create books database replicas

--- books_database/src/app.py
+++ books_database/src/app.py
@@ -18,7 +18,15 @@ from concurrent import futures
 # books_database_pb2_grpc.BooksDatabaseServiceServicer
 class BooksDatabaseService(books_database_grpc.BooksDatabaseServiceServicer):
     def __init__(self):
-       pass
+        self.id = int(os.getenv("DATABASE_ID"))
+        self.port = 50058 + self.id
+
+        self.all_addresses = {
+            1: "books_database1:50059",
+            2: "books_database2:50060",
+            3: "books_database3:50061",
+        }
+        self.all_addresses.pop(self.id)
 
 def serve():
     # Create a gRPC server
@@ -28,12 +36,12 @@ def serve():
     books_database_grpc.add_BooksDatabaseServiceServicer_to_server(BooksDatabaseService(), server)
 
     # Listen on port 50059
-    port = "50059"
-    server.add_insecure_port("[::]:" + port)
+    port = 50058 + int(os.getenv("DATABASE_ID", "1"))
+    server.add_insecure_port(f"[::]:{port}")
 
     # Start the server
     server.start()
-    print("Server started. Listening on port 50054.")
+    print(f"Database {os.getenv('DATABASE_ID')} Server started. Listening on port {port}.")
 
     # Keep thread alive
     server.wait_for_termination()
